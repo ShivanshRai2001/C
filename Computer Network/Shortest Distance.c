dd4c51b0b7726a2ce77cbe23cb61e66d9691131d
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2022-09-07 19:38:01 +0530

    Create Shortest Distance.c

---
+++ Computer Network/Shortest Distance.c
@@ -0,0 +1,106 @@
+#include<stdio.h>
+
+int shortest(int distance[5], int source);
+
+int visited[5]={0,0,0,0,0};
+
+int main()
+{
+    int dist[5],pred[5];
+    int source,n,i,y,new_dist;
+    int adjt[5][5]={99,2,4,99,99,2,99,2,4,99,4,2,99,99,3,99,4,99,99,5,99,99,3,5,99};
+    
+    printf("\n Enter the no. of Nodes - ");
+    scanf("%d",&n);
+    
+    printf("\n Enter the Source Node - ");
+    scanf("%d",&source);
+    
+    for(i=0;i<n;i++)
+    {
+        dist[i]= adjt[source][i];
+    
+        if(dist[i]!=99)
+        {
+            pred[i]=source;
+        }   
+        else
+        {
+            pred[i]=99;
+        }
+    }
+
+    printf("\n");
+    for(i=0;i<n;i++)
+    {
+        printf(" %d ",dist[i]);  
+    }
+    
+    // visited[source] = 1;
+    // y = shortest(dist, n);
+    // printf("\n");
+    // printf("\n %d", y);
+
+    printf("\n");
+    do{
+        y = shortest(dist, n);
+        visited[y] = 1;
+
+        for(i=0; i<n; i++)
+        {
+            if(visited[i] == 0)
+            {
+                new_dist = dist[y] + adjt[y][i];
+                if(new_dist < dist[i])
+                {
+                    dist[i] = new_dist;
+                    pred[i] = y;
+                }
+            }
+        }
+
+        for(i=0; i<n; i++)
+        {
+            if(visited[i] == 0)
+            {
+                break;
+            }
+        }
+    }
+    while(i<n);
+
+    printf("\n");
+    printf(" Distance array :-");
+    for(i=0;i<n;i++)
+    {
+        printf(" %d ",dist[i]);  
+    }
+    
+    printf("\n");
+    printf("\n Predecesspr array :-");
+    for(i=0;i<n;i++)
+    {   
+        printf(" %d ",pred[i]);  
+    }
+}
+
+int shortest(int distance[5], int source)
+{
+    int i;
+    int min, x;
+    min = 99;
+    x = -1;
+
+    for(i=0; i<source; i++)
+    {
+        if(visited[i] == 0)
+        {
+            if(min > distance[i])
+            {
+                min = distance[i];
+                x = i;
+            }
+        }   
+    }
+    return x;
+}
